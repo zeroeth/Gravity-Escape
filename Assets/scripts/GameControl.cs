8e364261b1ae94646f9c146abbfc4fe65ce88931
Pirolf <yuchengtu.uwm@gmail.com>
2015-05-14 15:40:29 -0500

    end of game: out of viewport > 3s

--- Assets/scripts/GameControl.cs
+++ Assets/scripts/GameControl.cs
@@ -6,16 +6,38 @@ public class GameControl : MonoBehaviour {
 	public static PhysicsEngine physics;
 	public static UIControl uiControl;
 	public static GameControl self;
+	[HideInInspector]public Camera mainCamera;
 	public float prepTime;
+	public float maxOutOfViewportTime;
+	public float outOfViewportTime;
 	void Awake(){
 		prepTime = 5f;
+		maxOutOfViewportTime = 3f;
+		outOfViewportTime = 0f;
 		self = this;
+		mainCamera = Camera.main;
 	}
 	// Use this for initialization
 	void Start () {
 		uiControl.UpdateTimer(prepTime);
 	}
-	
+	void RocketInViewportTest(){
+		Vector2 rocketInViewport 
+			= mainCamera.WorldToViewportPoint(rocket.transform.position);
+		if(rocketInViewport.x < 0f || rocketInViewport.x > 1f 
+			|| rocketInViewport.y < 0f || rocketInViewport.y > 1f){
+			//start counting 3 seconds
+			outOfViewportTime += Time.deltaTime;
+		}else{
+			outOfViewportTime = 0f;
+		}
+	}
+	void OnGameOver(){
+		Time.timeScale = 0f;
+		physics.physicsStarted = false;
+		//start playing back trails
+		
+	}
 	// Update is called once per frame
 	void Update () {
 		//Count 5
@@ -33,6 +55,14 @@ public class GameControl : MonoBehaviour {
 				uiControl.HideTimerPanel();
 			}
 			physics.physicsStarted = true;
+			//if rocket is out of sight for more than 3 seconds
+			//game over
+			
+			RocketInViewportTest();
+			if(outOfViewportTime >= maxOutOfViewportTime){
+				OnGameOver();
+				return;
+			}
 			if(Input.GetKey("w")){
 				rocket.thrustMagnitude += 10f;
 			}else if(Input.GetKeyUp("w")){
