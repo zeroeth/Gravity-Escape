b2383eabda94cff6cc6da7346b69434f2cfe9bc9
Pirolf <yuchengtu.uwm@gmail.com>
2015-05-15 12:11:00 -0500

    landing planet in scene

--- Assets/scripts/LandingPlanet.cs
+++ Assets/scripts/LandingPlanet.cs
@@ -1,19 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LandStar : Star {
-
-	// Use this for initialization
-	void Start () {
-	
+public class LandingPlanet : Star {
+	public float maxImpulse; //max impulse allowed without crashing the rocket
+	void Awake(){
+		if(maxImpulse <= 0f){
+			maxImpulse = 2.6f;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnCollisionEnter(Collision collision){
+	void OnCollisionEnter2D(Collision2D collision){
 		if(collision.gameObject.tag == "Rocket"){
 			//check if mv is too large
 			if(collision.contacts == null || collision.contacts.Length == 0){
@@ -24,7 +19,11 @@ public class LandStar : Star {
 			Vector2 contactNormal = collision.contacts[0].normal;
 			//m times v, used to meaure impulse
 			Vector2 mv = rocketRigidbody.mass * rocketRigidbody.velocity;
-			
+			float impulseMagnitude = Mathf.Abs(Vector2.Dot(mv, contactNormal.normalized));
+			Debug.Log(impulseMagnitude);
+			if(impulseMagnitude > maxImpulse){
+				Debug.Log("crashed!");
+			}
 		}
 	}
 }
